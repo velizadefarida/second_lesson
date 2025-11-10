0c0f7c77b5e22b9a8e15decff044082f157e6413
velizadefarida <velizadefarida@gmail.com>
2025-11-10 22:14:41 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+
+
+struct IntArray {
+  void add(int i);
+  int get(size_t id) const;
+  size-t size() const;
+} 
+
+
 int main()
 {
   int next = 0;
@@ -6,5 +15,12 @@ int main()
   if (!std::cin)
   {
     return 1;
+  }
+  IntArray a;
+  a.add(next);
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    int d = a.get(i);
+    //
   }
 }
