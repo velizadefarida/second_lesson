a5be57a0ee1d63bbd2ea24b832e1f118fdcfdfab
velizadefarida <velizadefarida@gmail.com>
2025-11-10 22:37:13 +0300

    5 commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ struct IntArray {
   int get(size_t id) const;
   size-t size() const;
   int last() const;
+  IntArray(int i);
+  ~IntArray();
 }; 
 
 
@@ -13,20 +15,27 @@ int main()
 {
   int next = 0;
   std::cin >> next;
-  IntArray a;
-  a.add(next);
-  while (std::cin >> next)
-  {
-  a.add(next);
+  try {
+    IntArray a(next);
+    //IntArray a; конструктор по умолчанию
+    //a.add(next);
+    while (std::cin >> next)
+    {
+      a.add(next);
+    }
+    if (std::cin.fail())
+    {
+      return 1;
+    }
+    size_t count = 1;
+    for (size_t i = 0; i < a.size() - 1; ++i)
+    {
+      int d = a.get(i);
+      count += !(d % a.last());
+    }
+    std::cout << count << "\n"
   }
-  if (std::cin.fail())
-  {
-    return 1;
-  }
-  size_t count = 1;
-  for (size_t i = 0; i < a.size() - 1; ++i)
-  {
-    int d = a.get(i);
-    count += !(d % a.last());
+  catch (const std::bad_alloc) {
+    return 2;
   }
 }
